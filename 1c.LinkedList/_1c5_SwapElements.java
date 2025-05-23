6b21df6c2ae39cfcf69c1465b9c48fc6125a6399
K VEERESH <kveeresh288@gmail.com>
2025-05-23 16:18:26 +0530

    Readme 1c5 Output

--- 1c.LinkedList/_1c5_SwapElements.java
+++ 1c.LinkedList/_1c5_SwapElements.java
@@ -1,4 +1,7 @@
-// 1c 5. Write a Java program that swaps two elements (first and third elements) in a linked list (using Collections.swap(l_list, 0, 2))
+// 1c 5. Write a Java program that swaps two elements (first and third elements) in a 
+//  linked list (using Collections.swap(l_list, 0, 2))
+
+//code :
 
 import java.util.*;
 
