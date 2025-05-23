1b05ba2409a7b985a26866056f25ba11be57389c
K VEERESH <kveeresh288@gmail.com>
2025-05-23 10:33:39 +0530

    1b 1 files added

---
+++ 1b.ArrayList/_1b_ColorSearch.java
@@ -0,0 +1,19 @@
+// 1. Write a java program for getting different colors through ArrayList interface and search whether
+// the color "Red" is available or not
+
+// code:
+ 
+import java.util.*;
+
+public class _1b_ColorSearch {
+    public static void main(String[] args) {
+        ArrayList<String> colors = new ArrayList<>(Arrays.asList("Red", "Green", "Blue", "Yellow"));
+
+        System.out.println("Colors: " + colors);
+        if (colors.contains("Red")) {
+            System.out.println("\"Red\" is available in the list.");
+        } else {
+            System.out.println("\"Red\" is not in the list.");
+        }
+    }
+}
