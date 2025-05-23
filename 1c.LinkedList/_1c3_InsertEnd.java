c8c41a900bf883552ebfe2e0e59cc0bc0cb2a4d1
K VEERESH <kveeresh288@gmail.com>
2025-05-23 16:07:31 +0530

    cd /Users/kveeresh/Desktop/Advance-Java-Programming-Assignment/1c.LinkedList/ && javac _1c2_ReverseIteration.java && java _1c2_ReverseIteration 3 Code Updated with proper Output

--- 1c.LinkedList/_1c3_InsertEnd.java
+++ 1c.LinkedList/_1c3_InsertEnd.java
@@ -1,5 +1,4 @@
 // 1c 3. Write a Java program to insert the specified element at the end of a linked list. (using l_listobj.offerLast("Pink"))
-
 //code:
 
 import java.util.*;
@@ -7,8 +6,10 @@ import java.util.*;
 public class _1c3_InsertEnd {
     public static void main(String[] args) {
         LinkedList<String> list = new LinkedList<>(Arrays.asList("Red", "Green", "Blue"));
+        System.out.println("List before"+ list);
         list.offerLast("Pink");
-
+        System.out.println("List After Last Offer (pink) : "+ list);
+        System.out.println();
         System.out.println("Updated List: " + list);
     }
 }
