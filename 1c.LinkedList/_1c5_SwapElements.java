c464ce5d209ab10806cf63e95c181643512b95c2
K VEERESH <kveeresh288@gmail.com>
2025-05-23 16:15:59 +0530

    1c5 is added with code and output link in Readme File

---
+++ 1c.LinkedList/_1c5_SwapElements.java
@@ -0,0 +1,13 @@
+// 1c 5. Write a Java program that swaps two elements (first and third elements) in a linked list (using Collections.swap(l_list, 0, 2))
+
+import java.util.*;
+
+public class _1c5_SwapElements {
+    public static void main(String[] args) {
+        LinkedList<String> list = new LinkedList<>(Arrays.asList("Red", "Green", "Blue", "Yellow"));
+        System.out.println("Before Swap: " + list);
+
+        Collections.swap(list, 0, 2); // Swap 1st and 3rd
+        System.out.println("After Swap: " + list);
+    }
+}
